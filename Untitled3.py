489c5698b5a136db15c77e9a8efcaeb03d457b7a
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-12-26 17:58:14 +0530

    Update Untitled3.py

--- Untitled3.py
+++ Untitled3.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
+#1.line plot
 import matplotlib.pyplot as plt
 marks = [88,57,59,78,90,55,14,88]
 div = [1,2,3,4,5,6,7,8]
@@ -11,24 +6,29 @@ plt.plot(div,marks)
 plt.show()
 
 
-# In[8]:
-
-
-len(marks)
-
-
-# In[10]:
-
-
+#2.scatter plot
 import matplotlib.pyplot as plt
 marks = [88,57,59,78,90,55,14,88]
 div = [1,2,3,4,5,6,7,8]
 plt.scatter(div,marks)
 plt.show()
 
+#3.Histogram 
+import matplotlib.pyplot as plt
+runs = [66,86,45,21,77,1,5,99,105,81,105,200]
+plt.hist(runs,bins=4)
+plt.show()
 
-# In[ ]:
-
+#Customization
+import matplotlib.pyplot as plt
+marks = [88,57,59,78,90,55,14,88]
+div = [1,2,3,4,5,6,7,8]
+plt.plot(div,marks)
+plt.xlabel('divisin')
+plt.ylabel('marks')
+plt.title('Marks obtained')
+plt.yticks([0,2,4,6,8])
+plt.show()
 
 
 
