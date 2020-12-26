42d0b648b73d4f80a6878c5dbbabf6a94d7c979f
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2020-12-26 11:38:02 +0530

    Add files via upload
    Basic plot with matplot

---
+++ Untitled3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+marks = [88,57,59,78,90,55,14,88]
+div = [1,2,3,4,5,6,7,8]
+plt.plot(div,marks)
+plt.show()
+
+
+# In[8]:
+
+
+len(marks)
+
+
+# In[10]:
+
+
+import matplotlib.pyplot as plt
+marks = [88,57,59,78,90,55,14,88]
+div = [1,2,3,4,5,6,7,8]
+plt.scatter(div,marks)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
